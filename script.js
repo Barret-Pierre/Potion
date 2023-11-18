93179280d7fb97fe7ed5f16da2c5d082133b587e
Barret-Pierre <pierrealexandrebarret.pro@gmail.com>
2023-11-18 01:15:13 +0100

    CSS : add potion and visual

--- script.js
+++ script.js
@@ -2,24 +2,25 @@ let potions = document.querySelectorAll(".potion");
 let button = document.querySelector("button");
 let alertTryAgain = document.querySelector(".alert-danger");
 let solution = document.querySelector(".alert-success");
+screen.orientation.lock("landscape");
 
 window.addEventListener("load", () => {
   for (const potion of potions) {
-    potion.style.backgroundColor = "black";
+    potion.style.fill = "black";
   }
 });
 
 function nextColor(actualColor) {
   const potionsColor = [
-    "red",
-    "green",
-    "black",
-    "blue",
-    "yellow",
-    "purple",
-    "pink",
+    "rgb(222, 26, 24)",
+    "rgb(240, 130, 56)",
+    "rgb(244, 194, 8)",
+    "rgb(42, 100, 108)",
+    "rgb(101, 74, 142)",
+    "rgb(205, 11, 91)",
+    "rgb(239, 132, 167)",
   ];
-
+  console.log(actualColor);
   const actualIndex = potionsColor.findIndex((color) => color === actualColor);
   if (actualIndex === potionsColor.length - 1) {
     return potionsColor[0];
@@ -34,26 +35,27 @@ for (const potion of potions) {
     if (!alertTryAgainClass.includes("d-none")) {
       alertTryAgain.setAttribute("class", `${alertTryAgainClass} d-none`);
     }
-    const actualColor = potion.style.backgroundColor;
+    const actualColor = potion.style.fill;
     const newColor = nextColor(actualColor);
-    potion.style.backgroundColor = newColor;
+    potion.style.fill = newColor;
   });
 }
 
 function validate() {
   const potionsColor = [];
   const solution = [
-    "blue",
-    "red",
-    "black",
-    "yellow",
-    "pink",
-    "green",
-    "purple",
+    "rgb(101, 74, 142)",
+    "rgb(239, 132, 167)",
+    "rgb(205, 11, 91)",
+    "rgb(222, 26, 24)",
+    "rgb(240, 130, 56)",
+    "rgb(42, 100, 108)",
+    "rgb(244, 194, 8)",
   ];
   for (const potion of potions) {
-    potionsColor.push(potion.style.backgroundColor);
+    potionsColor.push(potion.style.fill);
   }
+  console.log(potionsColor);
   if (potionsColor.join("-") === solution.join("-")) {
     return true;
   } else {
